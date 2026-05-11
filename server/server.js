4f8ff0bd5997c0f3b9c8f81e19375d50dea41ddd
VarnikaSharma2176 <varnikasharma2006@gmail.com>
2026-05-11 19:17:30 +0530

    feat: implement user authentication system
    - Added user authentication APIs
    - Implemented user registration and login
    - Added MongoDB user model
    - Added password hashing using bcrypt
    - Added JWT token generation
    - Configured authentication routes
    - Verified backend authentication flow

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 connectDB();
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
     message: "AI Study Planner API Running",
   });
 });
-
+app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
